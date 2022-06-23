2b2e7db3ff6bc087413da7014a9e6bab7115a818
Henryk Kubiczak <henryk.kubiczak@redge.com>
2022-06-23 17:30:38 +0200

    Add something

--- app/src/main/java/com/endeline/mymovie/ui/mymovie/ui/gui/movies/MoviesScreen.kt
+++ app/src/main/java/com/endeline/mymovie/ui/mymovie/ui/gui/movies/MoviesScreen.kt
@@ -1,10 +1,109 @@
 package com.endeline.mymovie.ui.mymovie.ui.gui.movies
 
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.annotation.FloatRange
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.FlingBehavior
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.google.accompanist.pager.*
+import kotlinx.coroutines.launch
 
 @Composable
 fun MoviesScreen(controller: NavHostController) {
-    Text(text = "Movies Screen")
+    CombinedTab()
+
+}
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun CombinedTab() {
+    val tabData = listOf(
+        "MUSIC" to Icons.Filled.Home,
+        "MARKET" to Icons.Filled.ShoppingCart,
+        "FILMS" to Icons.Filled.AccountBox,
+        "BOOKS" to Icons.Filled.Settings,
+    )
+    val pagerState = rememberPagerState(
+        pageCount = tabData.size,
+        initialOffscreenLimit = 2,
+        infiniteLoop = true,
+        initialPage = 1,
+    )
+    val tabIndex = pagerState.currentPage
+    val coroutineScope = rememberCoroutineScope()
+    Column {
+        TabRow(
+            selectedTabIndex = tabIndex,
+            indicator = { tabPositions ->
+                TabRowDefaults.Indicator(
+//                    Modifier.pagerTabIndicatorOffset(pagerState, tabPositions)
+                )
+            }
+        ) {
+            tabData.forEachIndexed { index, pair ->
+                Tab(selected = tabIndex == index, onClick = {
+                    coroutineScope.launch {
+                        pagerState.animateScrollToPage(index)
+                    }
+                }, text = {
+                    Text(text = pair.first)
+                }, icon = {
+                    Icon(imageVector = pair.second, contentDescription = null)
+                })
+            }
+        }
+        HorizontalPager(
+            state = pagerState,
+            modifier = Modifier.weight(1f)
+        ) { index ->
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = tabData[index].first,
+                )
+            }
+        }
+    }
+}
+
+
+
+//----------------
+
+@Composable
+fun LatestMovieScreen() {
+    Text(text = "Latest Movies Screen")
+}
+@Composable
+fun NowPlayingMovieScreen() {
+    Text(text = "Now Playing Movies Screen")
+}
+@Composable
+fun PopularMovieScreen() {
+    Text(text = "Popular Movies Screen")
+}
+@Composable
+fun TopRatedMovieScreen() {
+    Text(text = "TopRated Movies Screen")
+}
+
+@Composable
+fun UpcomingMovieScreen() {
+    Text(text = "Upcoming Movies Screen")
 }
